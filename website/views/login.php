253d7e738597f4f5a0b1b3eea30b32894338520b
Miguel Reis R00220964 <m.ferreira-nogueira-reis@mycit.ie>
2023-11-05 13:13:59 +0000

    Scroll now possible on menus. Fixed input windows so they are all evenly spaced. Fixed typo in registration window.

--- website/views/login.php
+++ website/views/login.php
@@ -6,8 +6,10 @@
       <div id="container">
         <h2>Login</h2>
         <form action="../includes/login.inc.php" method="POST">
-          <p><label>Email*:&nbsp;&nbsp;&nbsp;&nbsp;<input type="email" name="email" autocomplete="on"></label></p>
-          <p><label>Password*:&nbsp;<input type="password" name="password" required></label></p>
+          <p>
+            <label>Email*:&nbsp;&nbsp;&nbsp;&nbsp;<input type="email" name="email" autocomplete="on"></label>
+            <label>Password*:&nbsp;<input type="password" name="password" required></label>
+          </p>
           <p>Fields marked with * are mandatory</p>
         </form>
         <p><a href="menu.php">Login</a></p>
--- website/views/register.php
+++ website/views/register.php
@@ -4,15 +4,17 @@
   
     <main>
       <div id="container">
-        <h2>Login</h2>
+        <h2>Register</h2>
         <p>
         <form action="../includes/register.inc.php" method="POST">
-            <p><label>Name*:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="name" required autocomplete="on"></label></p>
-            <p><label>Email*:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="email" name="email" autocomplete="on"></label></p>
-            <p><label>IMC Number*:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="IMC" autocomplete="on"></label></p>
-            <p><label>Registration Number*:&nbsp;<input type="text" name="regNum" autocomplete="on"></label></p>
-            <p><label>Password*:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="password" name="password" required></label></p>
-            <p><label>Confirm Password*:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="password" name="confirm-password" required></label></p>
+            <p>
+              <label>Name*: <input type="text" name="name" required autocomplete="on"></label>
+              <label>Email*: <input type="email" name="email" autocomplete="on"></label>
+              <label>IMC Number*: <input type="text" name="IMC" autocomplete="on"></label>
+              <label>Registration Number*: <input type="text" name="regNum" autocomplete="on"></label>
+              <label>Password*: <input type="password" name="password" required></label>
+              <label>Confirm Password*: <input type="password" name="confirm-password" required></label>
+            </p>
             <p>Fields marked with * are mandatory</p>
             <p><button type="submit" name="submit">Register</button></p>
           </form>
