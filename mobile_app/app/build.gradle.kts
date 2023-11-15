fe5fa862c1e37864e737d9cf210330d53c9c2ca1
Alex Fenlon R00220236 <alex.fenlon@mycit.ie>
2023-11-15 13:29:57 +0000

    fixed bad gradle mistake - removed google ads for now

--- mobile_app/app/build.gradle.kts
+++ mobile_app/app/build.gradle.kts
@@ -10,6 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.LyfeRisk"
         minSdk = 29
+        //noinspection EditedTargetSdkVersion
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -33,7 +34,6 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.gms:play-services-ads:22.5.0")
     implementation("com.braintreepayments.api:braintree:3.20.1")
     implementation("com.github.kittinunf.fuel:fuel:2.3.1")
     implementation("com.stripe:stripe-android:20.34.4")
