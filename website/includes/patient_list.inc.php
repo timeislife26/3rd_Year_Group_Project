2285a01a298e37e35600727e152b9480077b6303
Miguel Reis R00220964 <m.ferreira-nogueira-reis@mycit.ie>
2023-11-19 22:13:45 +0000

    Created the php scripts for the predic and export Currenlty predic is just a copy of profile Export however already allows to safe a txt file Information saved is the same has the profile at the moment

--- website/includes/patient_list.inc.php
+++ website/includes/patient_list.inc.php
@@ -39,4 +39,65 @@ function displayPatientProfile($database, $patientList) {
         }
     }
 }
+
+// Function that will make the request for the prediction
+// (currently a copy of the Profile function, will be updated once the prediction is running)
+function displayPatientPrediction($database, $patientList) {
+  if (isset($_POST['generate'])) {
+      $refTable = "DoctorUsers";
+      $targetFullName = $_POST['selectedPatient'];
+      
+      $result = $database->getReference($refTable)->getValue();
+      
+      if ($result) {
+        foreach ($result as $entry) {
+          if (strcasecmp(trim($entry['fullName']), $targetFullName) === 0) {
+            // Match found, printing the entry data
+            echo "\n\tEmail Verified: " . $entry['emailVerified'] . "\n";
+            echo "\tFull Name: " . $entry['fullName'] . "\n";
+            echo "\tfID: " . $entry['fID'] . "\n";
+            echo "\tIMC: " . $entry['imc'] . "\n";
+            break; // Exit loop when match is found
+          }
+        }
+      } else {
+          echo "No results found for the specified table.";
+      }
+  }
+}
+
+// Function that will export the information of the patient into an txt file
+function displayPatientExport($database, $patientList) {
+  if (isset($_POST['export'])) {
+      $refTable = "DoctorUsers";
+      $targetFullName = $_POST['selectedPatient'];
+
+      $result = $database->getReference($refTable)->getValue();
+
+      if ($result) {
+          foreach ($result as $entry) {
+              if (strcasecmp(trim($entry['fullName']), $targetFullName) === 0) {
+                  // Match found, prepare the data for export
+                  $exportData = "Email Verified: " . $entry['emailVerified'] . "\n";
+                  $exportData .= "Full Name: " . $entry['fullName'] . "\n";
+                  $exportData .= "fID: " . $entry['fID'] . "\n";
+                  $exportData .= "IMC: " . $entry['imc'] . "\n";
+
+                  // Set the headers to trigger a download
+                  header('Content-Type: application/octet-stream');
+                  header('Content-Disposition: attachment; filename="patient_export.txt');
+                  header('Pragma: no-cache');
+                  header('Expires: 0');
+
+                  // Output the data
+                  echo $exportData;
+                  exit;
+              }
+          }
+          echo "No matching patient found.";
+      } else {
+          echo "No results found for the specified table.";
+      }
+  }
+}
 ?>
--- website/views/export.php
+++ website/views/export.php
@@ -1,16 +1,33 @@
 <?php
   include_once 'header.php';
+
+  include '../includes/patient_list.inc.php';
 ?>
   
     <main>
       <div id="container">
         <h2>Export Patient Information</h2>
         <p>
-          <form method="post">
-            <p><label>Patient Name*:&nbsp;<input type="text" name="firstname" required autocomplete="on"></label></p>
-          </form>
+          Select one of your patients from the drop down menu and click on "Export" to save the full 
+          profile of the selected person in a text file.
         </p>
-        <p><a href="">Export</a></p>
+        <form method="post">
+          <label for="patient_list">Patient Name*: 
+            <select id="patient_list" name="selectedPatient">
+              <?php
+              foreach ($patientList as $patient) {
+                echo '<option value="' . $patient . '">' , $patient , '</option>';
+              }
+              ?>
+            </select>
+            <button type="submit" name="export">Export</button>
+          </label>
+        </form>
+        <?php
+          if (isset($_POST['export'])) {
+            displayPatientExport($database, $patientList);
+          }
+        ?>
       </div>
     </main>
   </body>
