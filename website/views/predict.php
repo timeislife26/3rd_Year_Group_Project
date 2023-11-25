1db250320395f4defa9fdeb6db8225b90234d24e
timeislife26 <136696565+timeislife26@users.noreply.github.com>
2023-11-25 21:44:38 +0000

    The prediction page can now give a prediction for heart disease based on the information that is in the selected patients MedicalRecords document

--- website/views/predict.php
+++ website/views/predict.php
@@ -1,7 +1,23 @@
 <?php
   include_once 'header.php';
 
-  include '../includes/patient_list.inc.php';
+  // Database connection
+  require_once '../php-firebase/dbcon.php'; //'dbh.inc.php';
+  
+  // Query to fetch the list of names from DB
+  $refTable = "PatientUsers";
+  
+  $result = $database->getReference($refTable)->getValue();
+  
+  // Array to store names
+  $patientList = array();
+  $patientID = array();
+  
+  // Getting names and storing them in array
+  foreach ($result as $key => $row){
+      $patientList[] = $row["name"];
+      $patientID[] = $row["userId"];
+  }
 ?>
     
     <main>
@@ -11,16 +27,18 @@
           Select one of your patients from the drop down menu and click on "Generate" to receive a prediction 
           for the selected the selected person.
         </p>
-        <form method="post">
-          <label for="patient_list">Patient Name*: 
-            <select id="patient_list" name="selectedPatient">
-              <?php
-              foreach ($patientList as $patient) {
-                echo '<option value="' . $patient . '">' , $patient , '</option>';
-              }
-              ?>
+        <form action="../includes/predict.inc.php" method="POST">
+        <label for="patient_list">Patient Name*: 
+            <select id="patient_list" name="selectedPatient" onchange="updateSelectedUserID()">
+            <?php
+                    // Loop through names and corresponding userIDs
+                    for ($i = 0; $i < count($patientList); $i++) {
+                        echo '<option value="' . $patientID[$i] . '">' , $patientList[$i] , '</option>';
+                    }
+                    ?>
             </select>
-            <button type="submit" name="generate">Generate</button>
+            <input type="hidden" name="selectedUserID" id="selectedUserID">
+            <button type="submit" name="generate">Fill Form</button>
           </label>
         </form>
         <label id="textlabel"><p>Profile</p>
@@ -35,7 +53,20 @@
       </div>
     </main>
   </body>
+  <script>
+    var patientID;
 
+    window.onload = function() {
+        patientID = document.getElementById("patient_list").value;
+        console.log("Initial Patient ID: ", patientID);
+        document.getElementById("selectedUserID").value = patientID;
+    };
+    function updateSelectedUserID() {
+        patientID = document.getElementById("patient_list").value;
+        console.log("Selected Patient ID: ", patientID);
+        document.getElementById("selectedUserID").value = patientID;
+    }
+    </script>
 
 <?php
   include_once 'footer.php';
