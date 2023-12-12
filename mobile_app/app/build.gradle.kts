3e9047a2ff411954195e82a7e1038514eb7d0bff
Alex Fenlon R00220236 <fenlonalex@gmail.com>
2023-12-12 14:10:35 +0000

    FIXED GRADLE FINALLY.

--- mobile_app/app/build.gradle.kts
+++ mobile_app/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -10,7 +9,6 @@ android {
     defaultConfig {
         applicationId = "com.example.LyfeRisk"
         minSdk = 29
-        //noinspection EditedTargetSdkVersion
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -34,22 +32,13 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
-    implementation("com.google.zxing:core:3.4.1")
-    implementation("androidx.biometric:biometric:1.2.0-alpha05")
-    implementation("com.google.android.gms:play-services-ads:22.5.0")
-    implementation("com.braintreepayments.api:braintree:3.20.1")
-    implementation("com.github.kittinunf.fuel:fuel:2.3.1")
-    implementation("com.stripe:stripe-android:20.34.4")
-    implementation ("com.stripe:stripe-java:24.0.0")
-    implementation("com.stripe:stripe-android:20.34.4")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.5.3")
     implementation("androidx.navigation:navigation-ui:2.5.3")
     implementation("com.google.firebase:firebase-database:20.3.0")
-    implementation("com.google.firebase:firebase-auth:22.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
