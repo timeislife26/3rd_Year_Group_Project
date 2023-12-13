d5d8bb4db0e078e7bc1096c9a19384ada7c136fc
laiba1-crypto <115090887+laiba1-crypto@users.noreply.github.com>
2023-12-13 12:15:06 +0000

    Update MainActivity.java

--- mobile_app/app/src/main/java/com/example/LyfeRisk/MainActivity.java
+++ mobile_app/app/src/main/java/com/example/LyfeRisk/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.LyfeRisk;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,17 +10,10 @@ import com.paypal.android.sdk.payments.PayPalService;
 
 public class MainActivity extends AppCompatActivity {
 
-    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        Intent intent = new Intent(this, PayPalService.class);
-        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,
-                new PayPalConfiguration().environment(PayPalConfiguration.ENVIRONMENT_SANDBOX));
-        startService(intent);
-
 
         Button loginBtn = findViewById(R.id.loginBtn);
         loginBtn.setOnClickListener(new View.OnClickListener() {
@@ -33,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         Button registerBtn = findViewById(R.id.registerBtn);
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,11 +32,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent2);
             }
         });
-    }
-
-    @Override
-    protected void onDestroy() {
-        stopService(new Intent(this, PayPalService.class));
-        super.onDestroy();
     }
 }
