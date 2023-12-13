50c81653d81017522b9ef9f6d3970bf8c0bac9bd
laiba1-crypto <115090887+laiba1-crypto@users.noreply.github.com>
2023-12-13 12:15:49 +0000

    Update PaymentActivity.java

--- mobile_app/app/src/main/java/com/example/LyfeRisk/PaymentActivity.java
+++ mobile_app/app/src/main/java/com/example/LyfeRisk/PaymentActivity.java
@@ -1,5 +1,4 @@
 package com.example.LyfeRisk;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,57 +13,42 @@ import java.math.BigDecimal;
 public class PaymentActivity extends Activity {
 
     private static final String PAYPAL_CLIENT_ID = "ASz2PyFUD-2oIrvM_7HdLP7JLT1KLPDFoaBgeuj_2tZVJxrMRY0BgWZPycX0U4hogIBVRal8tGf3xjDY";
-
-
-    public static final String EXTRA_PAYMENT = "EXTRA_PAYMENT";
-    public static final String EXTRA_RESULT_CONFIRMATION = "EXTRA_RESULT_CONFIRMATION";
-    public static final int RESULT_EXTRAS_INVALID = 123;
+    private static final int REQUEST_CODE_PAYMENT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
+        setContentView(R.layout.activity_payment);
         Intent intent = new Intent(this, PayPalService.class);
         intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,
-                new PayPalConfiguration().environment(PayPalConfiguration.ENVIRONMENT_SANDBOX));
+                new PayPalConfiguration().environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+                        .clientId(PAYPAL_CLIENT_ID));
         startService(intent);
-
-
         PayPalPayment payment = new PayPalPayment(new BigDecimal("5.00"), "USD", "Premium Subscription",
                 PayPalPayment.PAYMENT_INTENT_SALE);
-
         Intent paymentIntent = new Intent(this, com.paypal.android.sdk.payments.PaymentActivity.class);
         paymentIntent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,
-                new PayPalConfiguration().environment(PayPalConfiguration.ENVIRONMENT_SANDBOX));
-        paymentIntent.putExtra(EXTRA_PAYMENT, payment);
-
-        startActivityForResult(paymentIntent, 0);
+                new PayPalConfiguration().environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+                        .clientId(PAYPAL_CLIENT_ID));
+        paymentIntent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
+        startActivityForResult(paymentIntent, REQUEST_CODE_PAYMENT);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == Activity.RESULT_OK) {
-
-            Toast.makeText(this, "Payment successful", Toast.LENGTH_SHORT).show();
-
-            // TODO: Update user's subscription status
-        } else if (resultCode == Activity.RESULT_CANCELED) {
-
-            Toast.makeText(this, "Payment canceled", Toast.LENGTH_SHORT).show();
-        } else if (resultCode == RESULT_EXTRAS_INVALID) {
-
-            Toast.makeText(this, "Invalid payment", Toast.LENGTH_SHORT).show();
+        if (requestCode == REQUEST_CODE_PAYMENT) {
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(this, "Payment successful", Toast.LENGTH_SHORT).show();
+                // TODO: Update user's subscription status or perform other actions
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(this, "Payment canceled", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == com.paypal.android.sdk.payments.PaymentActivity.RESULT_EXTRAS_INVALID) {
+                Toast.makeText(this, "Invalid payment", Toast.LENGTH_SHORT).show();
+            }
         }
-
-        finish();
-    }
-
-    @Override
-    protected void onDestroy() {
         stopService(new Intent(this, PayPalService.class));
-        super.onDestroy();
+        finish();
     }
 }
