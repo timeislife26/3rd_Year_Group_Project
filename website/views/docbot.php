d964edb38d761c5796d42af280c1a10ef871855b
Miguel Reis R00220964 <m.ferreira-nogueira-reis@mycit.ie>
2023-12-17 15:04:58 +0000

    Changedd look of docbot page.

--- website/views/docbot.php
+++ website/views/docbot.php
@@ -1,45 +1,43 @@
 <?php
   include_once 'header.php';
-  ?>
+?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Form</title>
-</head>
-<body>
-        <label for="bigTextBox">Big Text Box:</label><br>
-        <textarea id="bigTextBox" name="bigTextBox" rows="5" cols="50"></textarea><br><br>
+        <main>
+            <div id="container">
+                <h2>DocBot</h2>
 
-        <label for="inputBox">Input Box:</label>
-        <input type="text" id="inputBox" name="inputBox"><br><br>
+                <p>
+                    Please use the text box below to send your query to our AI doctor which will generate an answer from 
+                    our knowledge database to help assist with any issues you might have.
+                </p>
+                <label for="inputBox">Enter your query:<input type="text" id="inputBox" name="inputBox"></label>
+                <p><button onclick="getDocBotResponse()" name="send">Ask</button></p>
 
-        <button onclick="getDocBotResponse()" name="send">Send</button>
+                <label id="textlabel" for="bigTextBox"><p>DocBot Reply:</p>
+                    <textarea id="bigTextBox" name="bigTextBox" rows="10" cols="1600"></textarea>
+                </label>
+            </div>
+        </main>
+    </body>
 
-</body>
-</html>
-<script>
+    <script>
         //Ajax
-    function getDocBotResponse(){
-        message = document.getElementById("inputBox").value;
-        var x = new XMLHttpRequest();
-        var url = "../includes/docbot.inc.php?message=" + message;
+        function getDocBotResponse(){
+            message = document.getElementById("inputBox").value;
+            var x = new XMLHttpRequest();
+            var url = "../includes/docbot.inc.php?message=" + message;
         
-        console.log("Sent Message: ", message);
+            console.log("Sent Message: ", message);
 
-
-        x.open("GET", url);
-            x.onreadystatechange = function () {
-            if (x.readyState == 4 && x.status == 200) {
-                // Handle the response if needed
-                document.getElementById("bigTextBox").value = x.responseText;
-            }
+            x.open("GET", url);
+                x.onreadystatechange = function () {
+                if (x.readyState == 4 && x.status == 200) {
+                    // Handle the response if needed
+                    document.getElementById("bigTextBox").value = x.responseText;
+                }
             };
-        x.send();
+            x.send();
         }
-
     </script>
 
 <?php
