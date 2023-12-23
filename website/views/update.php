9072262e1afa31cb369d53105de17ff99241e1ea
Miguel Reis R00220964 <m.ferreira-nogueira-reis@mycit.ie>
2023-12-23 14:21:42 +0000

    Minor syntax and function fixes to update page and connected files. Form filling still not working.

--- website/views/update.php
+++ website/views/update.php
@@ -82,149 +82,128 @@ if (isset($_POST["update"])) {
         </form>
         <form action="../includes/update.inc.php" method="POST">
             <p>
-                <label>Name: <input type="text" name="name" required autocomplete="on"></label>
-                <label>Email: <input type="email" name="email" autocomplete="on"></label>
-                <label>Age: <input type="number" name="Age" min="0" max="125" required autocomplete="on"></label>
+                <label>Name: <input type="text" name="name" id="name" required autocomplete="on"></label>
+                <label>Email: <input type="email" name="email" id="email" autocomplete="on"></label>
+                <label>Age: <input type="number" name="Age" id="Age" min="0" max="125" required autocomplete="on"></label>
                 <fieldset>
                     <legend>Gender at Birth:</legend>
                     <div class="radio_btn">
-					            <label><input type="radio" name="gender" value="True">Male</label>
-					            <label><input type="radio" name="gender" value="False">Female</label>
+					            <label><input type="radio" name="gender" id="genderM" value="True">Male</label>
+					            <label><input type="radio" name="gender" id="genderF" value="False">Female</label>
                     </div>
                 </fieldset>
-                <label>Resting Blood Pressure: <input type="number" name="trewstbps" required autocomplete="on"></label>
-                <label>Serum Cholestoral: <input type="number" name="chol" required autocomplete="on"></label>
-                <label>Fasting Blood Sugar: <input type="number" name="fbs" required autocomplete="on"></label>
-                <label>Resting Electocardiographic Results: <input type="number" name="restecg" min="0" max="2" required autocomplete="on"></label>
-                <label>Maximum Heart Rate Achieved: <input type="number" name="thalach" required autocomplete="on"></label>
-                <label>ST Depression Induced by Exercise Relative to Rest: <input type="number" name="oldpeak" required autocomplete="on"></label>
-                <label>Slope of the Peak Exercise ST Segment: <input type="number" name="slope" min="0" max="2" required autocomplete="on"></label>
-                <label>Number of Major Vessels Coloured by Flourosopy: <input type="number" name="ca" min="0" max="3" required autocomplete="on"></label>
-                <label>Body Mass Index: <input type="number" step="0.01" name="bmi" required autocomplete="on"></label>
-                <label>Hemoglobin A1c: <input type="number" step="any" name="HbA1c_level" required autocomplete="on"></label>
-                <label>Blood Glucose Level: <input type="number" name="blood_glucose_level" required autocomplete="on"></label>
+                <label>Resting Blood Pressure: <input type="number" name="trewstbps" id="trewstbps" required autocomplete="on"></label>
+                <label>Serum Cholestoral: <input type="number" name="chol" id="chol" required autocomplete="on"></label>
+                <label>Fasting Blood Sugar: <input type="number" name="fbs" id="fbs" required autocomplete="on"></label>
+                <label>Resting Electocardiographic Results: <input type="number" name="restecg" id="restecg" min="0" max="2" required autocomplete="on"></label>
+                <label>Maximum Heart Rate Achieved: <input type="number" name="thalach" id="thalach" required autocomplete="on"></label>
+                <label>ST Depression Induced by Exercise Relative to Rest: <input type="number" name="oldpeak" id="oldpeak" required autocomplete="on"></label>
+                <label>Slope of the Peak Exercise ST Segment: <input type="number" name="slope" id="slope" min="0" max="2" required autocomplete="on"></label>
+                <label>Number of Major Vessels Coloured by Flourosopy: <input type="number" name="ca" id="ca" min="0" max="3" required autocomplete="on"></label>
+                <label>Body Mass Index: <input type="number" step="0.01" name="bmi" id="bmi" required autocomplete="on"></label>
+                <label>Hemoglobin A1c: <input type="number" step="any" name="HbA1c_level" id="HbA1c_level" required autocomplete="on"></label>
+                <label>Blood Glucose Level: <input type="number" name="blood_glucose_level" id="blood_glucose_level" required autocomplete="on"></label>
                 <fieldset>
                     <legend>Exercise Induced Angina:</legend>
                     <div class="radio_btn">
-					            <label><input type="radio" name="exang" value="True">Yes</label>
-					            <label><input type="radio" name="exang" value="False">No</label>
+					            <label><input type="radio" name="exang" id="exangT" value="True">Yes</label>
+					            <label><input type="radio" name="exang" id="exangF" value="False">No</label>
                     </div>
                 </fieldset>
                 <fieldset>
                     <legend>Thal:</legend>
                     <div class="radio_btn">
-					    <label><input type="radio" name="thal" value="0">Normal</label>
-					    <label><input type="radio" name="thal" value="1">Fixed Defect</label>
-                      <label><input type="radio" name="thal" value="2">Reversable Defect</label>
-                      <label><input type="radio" name="thal" value="3">3</label>
+					    <label><input type="radio" name="thal" id="thal0" value="0">Normal</label>
+					    <label><input type="radio" name="thal" id="thal1" value="1">Fixed Defect</label>
+                      <label><input type="radio" name="thal" id="thal2" value="2">Reversable Defect</label>
+                      <label><input type="radio" name="thal" id="thal3" value="3">3</label>
                     </div>
                 </fieldset>
                 <fieldset>
                     <legend>Smoking:</legend>
                     <div class="radio_btn">
-					            <label><input type="radio" name="Smoking_history" value="0" onclick="toggleSmokingStatus(false)">No info</label>
+					            <label><input type="radio" name="Smoking_history" id="Smoking_historyF" value="0" onclick="toggleSmokingStatus(false)">No info</label>
 					            <label><input type="radio" name="Smoking_history" value="1" onclick="toggleSmokingStatus(false)">Never</label>
                                 <label><input type="radio" name="Smoking_history" value="2" onclick="toggleSmokingStatus(false)">Formerly</label>
-					            <label><input type="radio" name="Smoking_history" value="3" onclick="toggleSmokingStatus(true)">Currently</label>
+					            <label><input type="radio" name="Smoking_history" id="Smoking_historyT" value="3" onclick="toggleSmokingStatus(true)">Currently</label>
 
                     </div>
                 </fieldset>
                 <fieldset>
                     <legend>Yellow Fingers:</legend>
                     <div class="radio_btn">
-					            <label><input type="radio" name="Yellow_Fingers" value="True">Yes</label>
-					            <label><input type="radio" name="Yellow_Fingers" value="False">No</label>
-                    </div>
-                </fieldset>
-                <fieldset>
-                    <legend>Alcohol Consuming:</legend>
-                    <div class="radio_btn">
-					    <label><input type="radio" name="Alcohol_Consuming" value="True">Yes</label>
-					    <label><input type="radio" name="Alcohol_Consuming" value="False">No</label>
-                    </div>
-                </fieldset>
-                <fieldset>
-                    <legend>Coughing:</legend>
-                    <div class="radio_btn">
-					    <label><input type="radio" name="Coughing" value="True">Yes</label>
-					    <label><input type="radio" name="Coughing" value="False">No</label>
-                    </div>
-                </fieldset>
-                <fieldset>
-                    <legend>Shortness of Breath:</legend>
-                    <div class="radio_btn">
-					    <label><input type="radio" name="Shortness_of_Breath" value="True">Yes</label>
-					    <label><input type="radio" name="Shortness_of_Breath" value="False">No</label>
+					            <label><input type="radio" name="Yellow_Fingers" id="Yellow_FingersT" value="True">Yes</label>
+					            <label><input type="radio" name="Yellow_Fingers" id="Yellow_FingersF" value="False">No</label>
                     </div>
                 </fieldset>
                 <fieldset>
                     <legend>Anxiety:</legend>
                     <div class="radio_btn">
-					            <label><input type="radio" name="Anxiety" value="True">Yes</label>
-					            <label><input type="radio" name="Anxiety" value="False">No</label>
+					            <label><input type="radio" name="Anxiety" id="AnxietyT" value="True">Yes</label>
+					            <label><input type="radio" name="Anxiety" id="AnxietyF" value="False">No</label>
                     </div>
                 </fieldset>
                 <fieldset>
                     <legend>Chronic Disease:</legend>
                     <div class="radio_btn">
-					            <label><input type="radio" name="Chronic_Disease" value="True">Yes</label>
-					            <label><input type="radio" name="Chronic_Disease" value="False">No</label>
+					            <label><input type="radio" name="Chronic_Disease" id="Chronic_DiseaseT" value="True">Yes</label>
+					            <label><input type="radio" name="Chronic_Disease" id="Chronic_DiseaseF" value="False">No</label>
                     </div>
                 </fieldset>
                 <fieldset>
                     <legend>Fatigue:</legend>
                     <div class="radio_btn">
-					            <label><input type="radio" name="Fatigue" value="True">Yes</label>
-					            <label><input type="radio" name="Fatigue" value="False">No</label>
+					            <label><input type="radio" name="Fatigue" id="FatigueT" value="True">Yes</label>
+					            <label><input type="radio" name="Fatigue" id="FatigueF" value="False">No</label>
                     </div>
                 </fieldset>
                 <fieldset>
                     <legend>Allergies:</legend>
                     <div class="radio_btn">
-					            <label><input type="radio" name="Allergy" value="True">Yes</label>
-					            <label><input type="radio" name="Allergy" value="False">No</label>
+					            <label><input type="radio" name="Allergy" id="AllergyT" value="True">Yes</label>
+					            <label><input type="radio" name="Allergy" id="AllergyF" value="False">No</label>
                     </div>
                 </fieldset>
                 <fieldset>
                     <legend>Wheezing:</legend>
                     <div class="radio_btn">
-					            <label><input type="radio" name="Wheezing" value="True">Yes</label>
-					            <label><input type="radio" name="Wheezing" value="False">No</label>
+					            <label><input type="radio" name="Wheezing" id="WheezingT" value="True">Yes</label>
+					            <label><input type="radio" name="Wheezing" id="WheezingF" value="False">No</label>
                     </div>
                 </fieldset>
                 <fieldset>
                     <legend>Difficulty Swallowing:</legend>
                     <div class="radio_btn">
-					            <label><input type="radio" name="Swallowing_Difficulty" value="True">Yes</label>
-					            <label><input type="radio" name="Swallowing_Difficulty" value="False">No</label>
+					            <label><input type="radio" name="Swallowing_Difficulty" id="Swallowing_DifficultyT" value="True">Yes</label>
+					            <label><input type="radio" name="Swallowing_Difficulty" id="Swallowing_DifficultyF" value="False">No</label>
                     </div>
                 </fieldset>
                 <fieldset>
                     <legend>Chest Pain:</legend>
                     <div class="radio_btn">
-					<label><input type="radio" name="Chest_pain" value="True" onclick="toggleChestPainType()">Yes</label>
-					<label><input type="radio" name="Chest_pain" value="False" onclick="toggleChestPainType()">No</label>
+					<label><input type="radio" name="Chest_pain" id="Chest_painT" value="True" onclick="toggleChestPainType()">Yes</label>
+					<label><input type="radio" name="Chest_pain" id="Chest_painF" value="False" onclick="toggleChestPainType()">No</label>
                     </div>
                 </fieldset>
                 <fieldset id="cpField" style="display: none;">
                     <legend>Chest Pain Type:</legend>
-                    <label><input type="radio" name="cp" value="0" checked>0</label>
-                    <label><input type="radio" name="cp" value="1">1</label>
-                    <label><input type="radio" name="cp" value="2">2</label>
-                    <label><input type="radio" name="cp" value="3">3</label>
+                    <label><input type="radio" name="cp" id="cp0" value="0" checked>0</label>
+                    <label><input type="radio" name="cp" id="cp1" value="1">1</label>
+                    <label><input type="radio" name="cp" id="cp2" value="2">2</label>
+                    <label><input type="radio" name="cp" id="cp3" value="3">3</label>
                 </fieldset>
                 <fieldset>
                     <legend>Hypertension:</legend>
                     <div class="radio_btn">
-					<label><input type="radio" name="hypertension" value="True">Yes</label>
-					<label><input type="radio" name="hypertension" value="False">No</label>
+					<label><input type="radio" name="hypertension" id="hypertensionT" value="True">Yes</label>
+					<label><input type="radio" name="hypertension" id="hypertensionF" value="False">No</label>
                     </div>
                 </fieldset>
                 <fieldset>
                     <legend>Heart Disease:</legend>
                     <div class="radio_btn">
-					<label><input type="radio" name="heart_disease" value="True">Yes</label>
-					<label><input type="radio" name="heart_disease" value="False">No</label>
+					<label><input type="radio" name="heart_disease" id="heart_diseaseT" value="True">Yes</label>
+					<label><input type="radio" name="heart_disease" id="heart_diseaseF" value="False">No</label>
                     </div>
                 </fieldset>
                 <input type="hidden" name="smokingStatus" id="smokingStatus" value="False">
